961aa7706d03b9cdd928749ad3c6758b25a95731
Aditya Hagawane <hagawaneaditya0506@gmail.com>
2024-06-21 13:51:11 +0530

    21/06/2024

---
+++ Example6.cs
@@ -0,0 +1,19 @@
+﻿/*using System;
+using School;
+namespace Csharp
+{
+    class School
+    {
+        static void Main(string[] args)
+        {
+            Student student = new School.Student();
+            student.display();
+            Teacher teacher = new School.Teacher();
+            teacher.display();
+            Department department = new School.Department();
+            department.display();
+
+            Console.ReadLine();
+        }
+    }
+}*/
---
+++ Example8.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+
+        public double Price {
+            get { return price; } 
+            set {
+                if (value < 0)
+                {
+                    throw new Exception("Price Must Be Greater Than \"O\"");
+                }
+                price = value; } 
+        }
+
+
+        public void SetName(string name)
+            { 
+            this.name = name; 
+        }
+        public void SetPrice(double price) {
+            if (price < 0)
+            {
+                throw new Exception("Price Can't be Negative");
+                
+            }
+            else
+            {
+                this.price = price;
+            }
+        }
+        public string GetName() { return this.name; }
+        public double GetPrice() { return this.price; }
+
+    }
+    internal class Example8
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.SetName("I Phone15");
+            product.Price = -20000;            
+            Console.WriteLine($"Product Name:{product.GetName()} \nProduct Price: {product.Price}");
+            
+            Console.ReadLine();
+        }
+    }
+}
